da1c5393c442677b5b80467829795fab16ca24f9
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2018-08-20 22:22:07 -0700

    Chapter 1 source code

---
+++ comments.c
@@ -0,0 +1,28 @@
+
+/* (1) A single-line C-style comment. */
+
+/* (2) A multi-line
+   C-style comment.   */
+
+/*
+ * (3) A very common
+ * format for a multi-line
+ * C-Style comment.
+ */
+
+/* (4) C-style comments can appear almost anywhere. */
+
+/*5*/ printf( /* say hello */ "Hello, world!\n");
+
+/*6*/ printf( "Hello, world!\n" ); /* yay! */
+
+// (7) A C++ style comment (terminated by End-of-Line).
+
+printf( "Hello, world!\n" ); // (8) Say hello; yay!
+
+//
+// (9) A more common way 
+// of commenting with multi-line
+// C++ style comments.
+//
+
---
+++ hello2.c
@@ -0,0 +1,16 @@
+/*
+ * hello2.c
+ * My first C program with comments.
+ * by <your name>
+ * on <year>/<month>/<day> 
+ */
+
+#include <stdio.h>
+
+int main()
+{
+  printf( "Hello, world!\n" );
+  return 0;
+}
+
+  /* eof */
