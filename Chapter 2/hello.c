839322674bf399097771ca7bd6089ba12633915e
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2018-09-02 21:37:22 -0700

    Update hello.c

--- Chapter 2/hello.c
+++ Chapter 2/hello.c
@@ -1,3 +1,10 @@
+//
+// hello.c
+// Chapter 2
+// <book title>
+//
+// Simple "Hello, world!" program.
+//
 
 #include <stdio.h>
 
@@ -6,3 +13,5 @@ int main()
   printf( "Hello, world!\n" );
   return 0;
 }
+
+//  <eof>
