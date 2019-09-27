7f3d63df4b4a58f997193cde0b6b86effb052823
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2019-09-26 20:57:58 -0700

    Add chapter 15 files

---
+++ showChar.c
@@ -0,0 +1,44 @@
+// showCar.c
+// Chapter 15
+// <book title>
+//
+// Demonstrate how to 
+// 1) create character variables
+// 2) assign character values as characters and as integer values
+// 3) print out an individual characer as 
+//    a) a character
+//    b) its decimal value
+//    c) its hexadecimal value
+//
+// compile with: cc showCar.c -o showCar -Wall -Werror -std=c11
+//
+
+
+#include <stdio.h>
+
+
+void showChar( int ch );
+
+int main( void ) 
+{
+    signed char aChar = 'A';
+           char c1    = 65 ; 
+           char c2    = 'a'; 
+           char c3    = 97 ; 
+           char c4    = '7';
+  unsigned char aByte = 7;
+
+  showChar( aChar );
+  showChar( c1 );
+  showChar( c2 );
+  showChar( c3 );
+  showChar( c4 );
+  showChar( aByte );
+}
+
+void showChar( int ch )
+{
+  printf( "ch = '%c' (%d) [%#x]\n" , ch , ch , ch ); 
+}
+
+  /* eof */
