ce8b0b71c00b538e1ae910e134bd7c19263a5b59
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2019-11-17 18:51:49 -0800

    Add files via upload

---
+++ Chapter 5/printLength.c
@@ -0,0 +1,32 @@
+// printLength.c
+// Chapter 5
+// <book title>
+// 
+// Program to print a length in given meters as well as 
+// in feed (converted from meters). Consideration must be 
+// given to values effectively 1.0 feet but which will never
+// be exactly 1.0 feet -- they'll be more like 1.0000000987 feet,
+// or 1.0 for any practical purpose.
+// We consider 1.0 feet within 4 significant digits.
+
+
+#include <stdio.h>
+
+void printLength( double meters );
+
+int main( void ) {
+  printLength( 0.0 );
+  printLength( 1.0 );
+  printLength( 12.0 / 39.67 );
+  printLength( 2.5 );
+}
+
+void printLength(  double meters ) {
+  double feet = meters * 39.67 / 12.0;
+  printf( "Length = %f meter%c\n" , 
+           meters, 
+           meters == 1.0 ? ' ' : 's'  );
+  printf( "Length = %f %s\n\n" , 
+          feet, 
+          0.99995 < feet && feet < 1.00005 ? "foot" : "feet" );
+}
