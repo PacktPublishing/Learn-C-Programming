5dcb60201a9c400e297f49a32984299a4ab62cb2
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2019-11-17 18:52:21 -0800

    Add files via upload

---
+++ Chapter 5/convertDigitToInt.c
@@ -0,0 +1,27 @@
+// convertDigitToInt.c
+// Chapter 5
+// <book title>
+// 
+// Program to convert digit characters to their
+// numerical values and add them together (operations on
+// characters).
+
+
+#include <stdio.h>
+
+int main( void ) {
+  char digit5 = '5';
+  char digit8 = '8';
+
+  int sumDigits = digit5 + digit8;
+  printf( "digit5 + digit8 = '5' + '8' = %d (oh, dear!)\n" ,
+        sumDigits );
+
+   char value5 = digit5 - '0';   // get the numerical value of '5'
+   char value8 = digit8 - '0';   // get the numerical value of '8'
+  sumDigits = value5 + value8;
+  printf( "value5 + value8 = 5 + 8 = %d\n" ,
+        sumDigits );
+}
+
+  // eof
