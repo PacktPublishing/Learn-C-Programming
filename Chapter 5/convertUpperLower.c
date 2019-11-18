159fee24b7df956b3ebafb8617fe792276959f31
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2019-11-17 18:55:13 -0800

    add convertUpperLower.c

---
+++ Chapter 5/convertUpperLower.c
@@ -0,0 +1,24 @@
+// convertUpperLower.c
+// Chapter 5
+// <book title>
+// 
+// Program to convert a lower case letter to upper case
+// and an upper case letter to lower case (operations on
+// characters).
+
+
+#include <stdio.h>
+
+int main( void ) 
+{
+  char lowerChar = 'b';
+  char upperChar = 'M';
+
+  char anUpper = lowerChar - 32;
+  char aLower  = upperChar + 32;
+
+  printf( "Lower case '%c' can be changed to upper case '%c'\n" ,
+           lowerChar , anUpper );
+  printf( "Upper case '%c' can be changed to lower case '%c'\n" ,
+           upperChar , aLower );
+}
