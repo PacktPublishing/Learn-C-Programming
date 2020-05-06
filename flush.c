e4c77b31f79bef29d4e178d8e7fb95c239adcb20
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2020-05-06 02:11:02 -0700

    1st chapter 21 file

---
+++ flush.c
@@ -0,0 +1,23 @@
+// flush.c
+// Chapter 21
+// Learn C Programming - Fundamentals of C
+//
+// Demonstrate input buffering works in 
+// "cooked mode" of console i/o.
+// Final output is flushed with \n or when program ends.
+//
+// compile with: cc flush.c -o flush -Wall -Werror -std=c11
+//
+
+#include <stdio.h>    // For printf()
+#include <unistd.h>   // For sleep()
+
+int main( void  )  {
+  printf( "You'll see this immediately.\nNow count to 5 slowly.\n");
+  printf( "Then, this appears ... ");
+  sleep( 5 );
+  printf( "when the buffer is finally flushed.\n" );
+}
+
+  // eof
+  
