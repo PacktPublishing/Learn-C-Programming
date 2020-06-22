a7f569e323b106be85341b99c25dc6b0332b060a
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2020-06-22 02:58:55 -0700

    remove spurious lines

--- Chapter11/array2.c
+++ Chapter11/array2.c
@@ -9,10 +9,7 @@
 #include <stdio.h>
 
 
-int main( void )
-{
-  int i, j, k;
-  i = j = k = 0;
+int main( void )  {
   int   anArray[10]  = {0}; // Initialize the whole thing to 0.
   int x, y , z;
   x = 11;
