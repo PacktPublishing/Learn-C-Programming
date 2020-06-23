0d5fb61100a451f2ea0368c277e9e9439eabc795
Jeff Szuhay <42553077+jeffszuhay@users.noreply.github.com>
2020-06-23 03:47:51 -0700

    update tombstone

--- Chapter02/hello_whitespace.c
+++ Chapter02/hello_whitespace.c
@@ -1,13 +1,16 @@
-//
 //  hello_whitespace.c
 //  Chapter 2
-//  <book title>
+//  Learn C Programming
 //
 //  Whitespace gone wild! 
 //  Just because you can do a thing does 
 //  mean that you must to that thing.
 //
-//  Don't do this, either.
+//  Don't do what you see here, either.
+//
+// Compile with:
+//
+//    cc hello_whitespace.c -Wall -Werror -std=c11
 //
 
 #             include                             <stdio.h>
